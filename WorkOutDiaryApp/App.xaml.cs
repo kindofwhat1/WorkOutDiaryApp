4df2c83482b8659dcd67bfaad50430dec0e1ba8e
Marc <fire_element_4@hotmail.com>
2021-01-05 20:53:33 +0100

    Add project files.

---
+++ WorkOutDiaryApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WorkOutDiaryApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+           MainPage mainPage = new MainPage();
+           MainPage = new NavigationPage(mainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ WorkOutDiaryApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkOutDiaryApp
+{
+   
+    public partial class MainPage : ContentPage
+    {
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Add_To_Diary_Clicked(object sender, EventArgs e)
+        {
+            NewDiary newDiary = new NewDiary();
+            newDiary.Title = "New Diary";
+            await Navigation.PushAsync(newDiary, true);// True skapar en animation
+            
+        }
+
+        private async void Button_Go_To_Diary_Clicked(object sender, EventArgs e)
+        {
+            Diary diary = new Diary();
+            diary.Title = "Diary";
+            await Navigation.PushAsync(diary, true);
+        }
+    }
+}
