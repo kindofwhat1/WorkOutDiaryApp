d9d6e78a380a4271d81055d820a15a99f13339b4
Marc <fire_element_4@hotmail.com>
2021-01-09 17:58:19 +0100

    Created Model and added SQLite nuget

---
+++ WorkOutDiaryApp/Model/DiaryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkOutDiaryApp.Model
+{
+    class DiaryModel
+    {
+        public int Id { get; set; }
+        public int Squats { get; set; }
+        public int PushUps { get; set; }
+        public int MountainClimbers { get; set; }
+        public int Burpees { get; set; }
+        public DateTime Time {get; set; } 
+        public Byte[] Image { get; set; }
+    }
+}
